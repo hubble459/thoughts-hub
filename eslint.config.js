fe7e4ed70f13f3f6471687b9e81e5b5b54956f83
Quentin Correia <qcorreia@mostwanted.io>
2024-07-29 11:43:15 +0200

    chore(app): initial commit

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginVue from 'eslint-plugin-vue';
+import withNuxt from './.nuxt/eslint.config.mjs';
+import stylistic from '@stylistic/eslint-plugin';
+
+export default withNuxt([
+    { files: ['**/*.{js,mjs,cjs,ts,vue}'], ignores: ['.nuxt/**/*', 'node_modules/**/*'] },
+    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...pluginVue.configs['flat/essential'],
+    { files: ['**/*.vue'], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+    stylistic.configs.customize({
+        indent: 4,
+        commaDangle: 'always-multiline',
+        quotes: 'single',
+        semi: true,
+        braceStyle: '1tbs',
+        quoteProps: 'consistent-as-needed',
+        arrowParens: true,
+        blockSpacing: true,
+    }),
+]);
